cd2c3eac707a596e8b5fea811ce6cdd4f90a2b1f
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-12-04 13:40:11 -0800

    Add "--diff-only" option
    This shows differences without deploying the new version. This is useful
    to run interactively (e.g. when modifying the config), but I'm also
    planning to make this run each day via Jenkins and email us if there are
    changes.
    
    This way people can make testing changes via the Marathon UI, but we get
    notified to eventually correct them if they forget to incorporate/revert
    them.

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import copy
 import getpass
 import json
@@ -7,7 +8,7 @@ from collections import namedtuple
 
 import yaml
 from cached_property import cached_property
-from ocflib.infra.mesos.marathon import MarathonClient
+from ocflib.infra.mesos import marathon
 from ocflib.misc import shell
 
 
@@ -97,68 +98,98 @@ def split_docker(tag):
         return tag, 'latest'
 
 
-def update_app(client, app):
+def diff_app(client, app):
     print(shell.bold('Comparing "{}" on-disk definition to the current status in Marathon:'.format(app.id)))
     deployed = client.app_status(app.id)['app']
     changes = diff_json(app.json, deployed)
 
     if not changes:
         print(shell.bold(shell.green('There were no differences.')))
+        return False
     else:
-        print(shell.bold(shell.yellow(
-            'There were differences. Starting a new deployment.',
+        print(shell.bold(shell.yellow('There were differences.')))
+        return True
+
+
+def update_app(client, app):
+    print(shell.bold(shell.yellow('Starting a new deployment.')))
+    deployed = client.app_status(app.id)['app']
+
+    # update the image tag (this doesn't quite match the on-disk tag)
+    on_disk_tag = split_docker(app.json['container']['docker']['image'])
+    deployed_tag = split_docker(deployed['container']['docker']['image'])
+
+    new_json = copy.deepcopy(app.json)
+
+    if on_disk_tag[0] == deployed_tag[0]:
+        # The repo didn't change, but the version did.
+        new_json['container']['docker']['image'] = '{}:{}'.format(*deployed_tag)
+    else:
+        # The repo changed, so reset to ${repo}:latest.
+        new_json['container']['docker']['image'] = '{}:{}'.format(*on_disk_tag)
+
+    client.deploy_app(app.id.lstrip('/'), new_json, report=unbuf_print)
+
+    # make sure there are no longer any differences
+    print(shell.bold('Confirming there are now no differences...'))
+    new_deployed = client.app_status(app.id)['app']
+    if diff_json(app.json, new_deployed):
+        print(shell.bold(shell.red(
+            'There were still differences in the on-disk and deployed version of "{}", '
+            'even after making a new deployment.\n'
+            '\n'
+            'This most likely means that the app config is missing some default value.\n'
+            "This isn't a huge problem, but it *does* mean that every time this repo gets pushed, "
+            'we do a no-op push which wastes time :(\n'
+            '\n'
+            'You should fix this, probably by taking the differences listed above and adding\n'
+            "them to the app's config on disk.".format(app.id)
         )))
-
-        # update the image tag (this doesn't quite match the on-disk tag)
-        on_disk_tag = split_docker(app.json['container']['docker']['image'])
-        deployed_tag = split_docker(deployed['container']['docker']['image'])
-
-        new_json = copy.deepcopy(app.json)
-
-        if on_disk_tag[0] == deployed_tag[0]:
-            # The repo didn't change, but the version did.
-            new_json['container']['docker']['image'] = '{}:{}'.format(*deployed_tag)
-        else:
-            # The repo changed, so reset to ${repo}:latest.
-            new_json['container']['docker']['image'] = '{}:{}'.format(*on_disk_tag)
-
-        client.deploy_app(app.id.lstrip('/'), new_json, report=unbuf_print)
-
-        # make sure there are no longer any differences
-        print(shell.bold('Confirming there are now no differences...'))
-        new_deployed = client.app_status(app.id)['app']
-        if diff_json(app.json, new_deployed):
-            print(shell.bold(shell.red(
-                'There were still differences in the on-disk and deployed version of "{}", '
-                'even after making a new deployment.\n'
-                '\n'
-                'This most likely means that the app config is missing some default value.\n'
-                "This isn't a huge problem, but it *does* mean that every time this repo gets pushed, "
-                'we do a no-op push which wastes time :(\n'
-                '\n'
-                'You should fix this, probably by taking the differences listed above and adding\n'
-                "them to the app's config on disk.".format(app.id)
-            )))
-            raise AssertionError('Deployment failed.')
-        else:
-            print(shell.bold(shell.green('OK!')))
+        raise AssertionError('Deployment failed.')
+    else:
+        print(shell.bold(shell.green('OK!')))
 
 
 def main():
-    creds_path = os.path.join(os.path.expanduser('~'), '.ocf-marathon')
-    if os.path.isfile(creds_path):
-        with open(creds_path) as f:
+    parser = argparse.ArgumentParser(description='deploy service configs')
+    parser.add_argument(
+        '--diff-only', action='store_true',
+        help='Only show differences (do not deploy), exiting nonzero if any exist.',
+    )
+    parser.add_argument('--marathon', default=marathon.MARATHON_URL)
+    parser.add_argument('--user', default=getpass.getuser())
+    parser.add_argument(
+        '--cred-path',
+        default=os.path.join(os.path.expanduser('~'), '.ocf-marathon'),
+    )
+    args = parser.parse_args()
+
+    # TODO: reduce this duplication with the ocf-marathon script in utils
+    if os.path.isfile(args.cred_path):
+        unbuf_print('Loading credentials from {}.'.format(args.cred_path))
+        with open(args.cred_path) as f:
             creds = json.load(f)
         user = creds['user']
         password = creds['password']
     else:
-        user = getpass.getuser()
-        password = getpass.getpass()
+        user = args.user
+        password = getpass.getpass("{}'s password: ".format(user))
 
-    client = MarathonClient(user, password)
+    client = marathon.MarathonClient(user, password, url=args.marathon)
 
+    found_diff = False
     for app in all_apps():
-        update_app(client, app)
+        if diff_app(client, app):
+            found_diff = True
+            if args.diff_only:
+                print(shell.bg_yellow('Not updating app, running in diff-only mode.'))
+            else:
+                update_app(client, app)
+
+    if found_diff and args.diff_only:
+        print(shell.bg_red('Differences were found!'))
+        return 1
+
 
 
 if __name__ == '__main__':
